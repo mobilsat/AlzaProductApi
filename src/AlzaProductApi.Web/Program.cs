98a519c5b3a7040fff6a56ab18a9f9ba7710e0c3
Ctibor Chlup <ctibor.chlup@mobilsat.cz>
2026-01-09 08:01:18 +0100

    Minor fixes and adjustments (duplicates, tests)

--- src/AlzaProductApi.Web/Program.cs
+++ src/AlzaProductApi.Web/Program.cs
@@ -13,10 +13,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 
 
@@ -73,9 +69,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
-
 app.Run();
 
 public partial class Program { }
--- tests/AlzaProductApi.Tests/ProductsControllerTests.cs
+++ tests/AlzaProductApi.Tests/ProductsControllerTests.cs
@@ -6,7 +6,6 @@ namespace AlzaProductApi.Tests;
 
 public class ProductsControllerTests
 {
-	
 	#region V1
 	[Fact]
 	public async Task V1_GetProducts_Returns200AndList()
@@ -30,31 +29,55 @@ public class ProductsControllerTests
 	}
 
 	[Fact]
-	public async Task V1_PatchDescription_UpdatesDescription_Returns204()
+	public async Task V1_PatchDescription_UpdatesDescription_Returns204_AndPersistsChange()
 	{
 		using var client = new ApiFactory().CreateClient();
 
-		var body = JsonContent.Create(new { description = "New description" });
-		var res = await client.PatchAsync("/api/v1/products/1/description", body);
+		// Arrange
+		var newDescription = "New description";
 
-		res.StatusCode.Should().Be(HttpStatusCode.NoContent);
+		// Act (PATCH)
+		var body = JsonContent.Create(new { description = newDescription });
+		var patchRes = await client.PatchAsync("/api/v1/products/1/description", body);
+
+		// Assert (PATCH response)
+		patchRes.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+		// Act (GET to verify persisted change)
+		var getRes = await client.GetAsync("/api/v1/products/1");
+		getRes.StatusCode.Should().Be(HttpStatusCode.OK);
+
+		var productJson = await getRes.Content.ReadAsStringAsync();
+		productJson.Should().Contain(newDescription);
 	}
 	#endregion
 
 	#region V2
 	[Fact]
-	public async Task V2_GetProducts_Paged_ReturnsHeaders()
+	public async Task V2_GetProducts_Paged_ReturnsHeaders_WithExpectedValues_AndLinkHeader()
 	{
 		using var client = new ApiFactory().CreateClient();
 
-		var res = await client.GetAsync("/api/v2/products?page=1&pageSize=2");
+		// Act
+		var res = await client.GetAsync("/api/v2/products?page=1&pageSize=4");
+
+		// Assert
 		res.StatusCode.Should().Be(HttpStatusCode.OK);
 
+		// Headers exist
 		res.Headers.Contains("X-Total-Count").Should().BeTrue();
 		res.Headers.Contains("X-Page").Should().BeTrue();
 		res.Headers.Contains("X-Page-Size").Should().BeTrue();
+
+		// Header values
+		res.Headers.GetValues("X-Total-Count").Single().Should().Be("10");
+		res.Headers.GetValues("X-Page").Single().Should().Be("1");
+		res.Headers.GetValues("X-Page-Size").Single().Should().Be("4");
+
+		// Link header for navigation
+		res.Headers.Contains("Link").Should().BeTrue();
+		res.Headers.GetValues("Link").Single().Should().Contain("rel=\"first\"");
+		res.Headers.GetValues("Link").Single().Should().Contain("rel=\"last\"");
 	}
 	#endregion
 }
-
-
